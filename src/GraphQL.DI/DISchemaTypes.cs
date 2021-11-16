b95160aa7f3be8988f1c1e912ff7238860121c1b
Shane32 <shane@acdmail.com>
2021-11-16 12:32:57 -0500

    Add AutoOutputObjectGraphType

--- src/GraphQL.DI/DISchemaTypes.cs
+++ src/GraphQL.DI/DISchemaTypes.cs
@@ -7,9 +7,14 @@ namespace GraphQL.DI
     /// <inheritdoc/>
     public class DISchemaTypes : SchemaTypes
     {
+        private readonly bool _autoMapInputTypes;
+        private readonly bool _autoMapOutputTypes;
+
         /// <inheritdoc/>
-        public DISchemaTypes(ISchema schema, IServiceProvider serviceProvider) : base(schema, serviceProvider)
+        public DISchemaTypes(ISchema schema, IServiceProvider serviceProvider, bool autoMapInputTypes, bool autoMapOutputTypes) : base(schema, serviceProvider)
         {
+            _autoMapInputTypes = autoMapInputTypes;
+            _autoMapOutputTypes = autoMapOutputTypes;
         }
 
         /// <inheritdoc/>
@@ -21,5 +26,17 @@ namespace GraphQL.DI
             }
             return type;
         }
+
+        /// <summary>
+        /// Return true if the specified CLR type should be wrapped with <see cref="AutoInputObjectGraphType{TSourceType}"/>
+        /// to create a graph type if no graph type is mapped for this input type.
+        /// </summary>
+        protected virtual bool AutoMapInputType(Type clrType) => _autoMapInputTypes;
+
+        /// <summary>
+        /// Return true if the specified CLR type should be wrapped with <see cref="AutoOutputObjectGraphType{TSourceType}"/>
+        /// to create a graph type if no graph type is mapped for this output type.
+        /// </summary>
+        protected virtual bool AutoMapOutputType(Type clrType) => _autoMapOutputTypes;
     }
 }
