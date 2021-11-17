96c9127f5bfca88447b107043ff689dfa16bd014
Shane32 <shane@acdmail.com>
2021-11-17 17:49:57 -0500

    Update tests

--- src/Tests/Execution/DISchemaTypes.cs
+++ src/Tests/Execution/DISchemaTypes.cs
@@ -1,5 +1,6 @@
 using System;
 using System.Collections.Generic;
+using GraphQL;
 using GraphQL.DI;
 using GraphQL.Types;
 using Moq;
@@ -13,7 +14,7 @@ namespace Execution
         [Theory]
         [InlineData(typeof(int), false, typeof(IntGraphType))]
         [InlineData(typeof(int), true, typeof(IntGraphType))]
-        [InlineData(typeof(Class1), false, null)]
+        [InlineData(typeof(Class1), false, typeof(AutoObjectGraphType<Class1>))]
         [InlineData(typeof(Class1), true, typeof(AutoInputObjectGraphType<Class1>))]
         public void GetGraphTypeFromClrType(Type clrType, bool isInputType, Type graphType)
         {
@@ -31,5 +32,32 @@ namespace Execution
         }
 
         private class Class1 { }
+
+        [Fact]
+        public void InputOutputTypesWork()
+        {
+            var schema = new Schema();
+            schema.Query = new ObjectGraphType();
+            var newField = new FieldType { Name = "Test", Type = typeof(GraphQLClrOutputTypeReference<Class1Model>) };
+            newField.Arguments = new QueryArguments(new QueryArgument<GraphQLClrInputTypeReference<Class2InputModel>> { Name = "arg" });
+            schema.Query.AddField(newField);
+            var schemaTypes = new DISchemaTypes(schema, new DefaultServiceProvider());
+            var class1Type = schemaTypes["Class1"].ShouldBeAssignableTo<IObjectGraphType>();
+            class1Type.Fields.Count.ShouldBe(1);
+            class1Type.Fields.Find("value").ShouldNotBeNull();
+            var class2Type = schemaTypes["Class2Input"].ShouldBeAssignableTo<IInputObjectGraphType>();
+            class2Type.Fields.Count.ShouldBe(1);
+            class2Type.Fields.Find("value").ShouldNotBeNull();
+        }
+
+        private class Class1Model
+        {
+            public int Value { get; set; }
+        }
+
+        private class Class2InputModel
+        {
+            public int Value { get; set; }
+        }
     }
 }
