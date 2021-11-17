27128746a032af97ac17616844a7c9fab1f09d44
Shane32 <shane@acdmail.com>
2021-11-17 17:53:19 -0500

    Update to tests

--- src/Tests/Execution/DISchemaTypes.cs
+++ src/Tests/Execution/DISchemaTypes.cs
@@ -25,10 +25,54 @@ namespace Execution
 
         private class MySchemaTypes : DISchemaTypes
         {
-            public MySchemaTypes() : base(new Schema(), Mock.Of<IServiceProvider>(), true, true) { }
+            public MySchemaTypes() : base(new Schema(), Mock.Of<IServiceProvider>()) { }
 
             public new Type GetGraphTypeFromClrType(Type clrType, bool isInputType, List<(Type ClrType, Type GraphType)> typeMappings)
                 => base.GetGraphTypeFromClrType(clrType, isInputType, typeMappings);
+        }
+
+        [Theory]
+        [InlineData(typeof(int), false, typeof(IntGraphType))]
+        [InlineData(typeof(int), true, typeof(IntGraphType))]
+        [InlineData(typeof(Class1), false, null)]
+        [InlineData(typeof(Class1), true, typeof(AutoInputObjectGraphType<Class1>))]
+        public void GetGraphTypeFromClrType_NoOutput(Type clrType, bool isInputType, Type graphType)
+        {
+            var mySchemaTypes2 = new MySchemaTypes2();
+            var mappedTypes = new List<(Type, Type)>();
+            mySchemaTypes2.GetGraphTypeFromClrType(clrType, isInputType, mappedTypes).ShouldBe(graphType);
+        }
+
+        private class MySchemaTypes2 : DISchemaTypes
+        {
+            public MySchemaTypes2() : base(new Schema(), Mock.Of<IServiceProvider>()) { }
+
+            public new Type GetGraphTypeFromClrType(Type clrType, bool isInputType, List<(Type ClrType, Type GraphType)> typeMappings)
+                => base.GetGraphTypeFromClrType(clrType, isInputType, typeMappings);
+
+            protected override bool AutoMapOutputType(Type clrType) => false;
+        }
+
+        [Theory]
+        [InlineData(typeof(int), false, typeof(IntGraphType))]
+        [InlineData(typeof(int), true, typeof(IntGraphType))]
+        [InlineData(typeof(Class1), false, typeof(AutoObjectGraphType<Class1>))]
+        [InlineData(typeof(Class1), true, null)]
+        public void GetGraphTypeFromClrType_NoInput(Type clrType, bool isInputType, Type graphType)
+        {
+            var mySchemaTypes3 = new MySchemaTypes3();
+            var mappedTypes = new List<(Type, Type)>();
+            mySchemaTypes3.GetGraphTypeFromClrType(clrType, isInputType, mappedTypes).ShouldBe(graphType);
+        }
+
+        private class MySchemaTypes3 : DISchemaTypes
+        {
+            public MySchemaTypes3() : base(new Schema(), Mock.Of<IServiceProvider>()) { }
+
+            public new Type GetGraphTypeFromClrType(Type clrType, bool isInputType, List<(Type ClrType, Type GraphType)> typeMappings)
+                => base.GetGraphTypeFromClrType(clrType, isInputType, typeMappings);
+
+            protected override bool AutoMapInputType(Type clrType) => false;
         }
 
         private class Class1 { }
