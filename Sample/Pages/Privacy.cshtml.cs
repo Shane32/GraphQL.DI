d0cf3e570892fe458a65f19ae787695eccdeb2a6
Shane32 <shane@acdmail.com>
2024-12-11 18:31:33 -0500

    update

--- Sample/Pages/Privacy.cshtml.cs
+++ Sample/Pages/Privacy.cshtml.cs
@@ -1,24 +1,10 @@
-﻿using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
-using Microsoft.Extensions.Logging;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 
-namespace Sample.Pages
+namespace Sample.Pages;
+
+public class PrivacyModel : PageModel
 {
-    public class PrivacyModel : PageModel
+    public void OnGet()
     {
-        private readonly ILogger<PrivacyModel> _logger;
-
-        public PrivacyModel(ILogger<PrivacyModel> logger)
-        {
-            _logger = logger;
-        }
-
-        public void OnGet()
-        {
-        }
     }
 }
